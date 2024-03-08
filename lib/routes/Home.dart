a74b875e7cf94c7562dcaee6b9a49fbea9eeca73
V3ntus <29584664+V3ntus@users.noreply.github.com>
2024-03-08 09:11:05 -0600

    feat: base home page

---
+++ lib/routes/Home.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(onPressed: () => context.go("/login"), child: Text("Login"));
+  }
+}
+
